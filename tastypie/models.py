2121fc1259ce29fbdfe1d4f4ed7483c0f7f5dd87
Ivan Chernov <ivan.chernov@ostrovok.ru>
2016-07-09 23:14:57 +0300

    [Feat] may custom tastypie

--- tastypie/models.py
+++ tastypie/models.py
@@ -29,7 +29,9 @@ class ApiAccess(models.Model):
 
 if 'django.contrib.auth' in settings.INSTALLED_APPS:
     import uuid
-    from tastypie.compat import AUTH_USER_MODEL
+
+    AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
     class ApiKey(models.Model):
         user = models.OneToOneField(AUTH_USER_MODEL, related_name='api_key')
         key = models.CharField(max_length=128, blank=True, default='', db_index=True)
